07ec92b518f8e44122f6bf14f77224deb05ab4e7
Zhang Hao <zhanghaohit@qq.com>
2024-07-27 15:09:11 +0800

    first format 适配google风格

--- src/Plan/traj_planner/include/path_searching/kino_model.hpp
+++ src/Plan/traj_planner/include/path_searching/kino_model.hpp
@@ -6,7 +6,6 @@
 #include <boost/bind.hpp>
 #include <iostream>
 
-
 #include <Eigen/Geometry>
 #include <Eigen/StdVector>
 #include "common/basics/basics.h"
@@ -16,67 +15,65 @@
 
 namespace odeint = boost::numeric::odeint;
 
-namespace  path_searching{
+namespace path_searching {
 
-  class KinoModel {
-  public:
+class KinoModel {
+ public:
 
-    KinoModel(){}
-    ~KinoModel(){}
+  KinoModel() {}
+  ~KinoModel() {}
 
-    void setParam(common::VehicleParam vp){
-  
-      vp_ = vp;
+  void setParam(common::VehicleParam vp) {
 
-    }
+    vp_ = vp;
 
-    void stateTransit(Eigen::Vector4d &state0,  Eigen::Vector4d &state1,
-              Eigen::Vector2d &ctrl_input, double dt) {
+  }
 
-      state_   = state0;
-      control_ = ctrl_input;
-      UpdateInternalState();
+  void stateTransit(Eigen::Vector4d &state0, Eigen::Vector4d &state1,
+                    Eigen::Vector2d &ctrl_input, double dt) {
 
-      odeint::integrate(boost::ref(*this), internal_state_, 0.0, dt, dt);
+    state_ = state0;
+    control_ = ctrl_input;
+    UpdateInternalState();
 
-      state_(0) = internal_state_[0];
-      state_(1) = internal_state_[1];
-      decimal_t theta_tmp = internal_state_[2];
-      //hzchzc
-      // theta_tmp -= (internal_state_[2]>= kPi) * 2 * kPi;
-      // theta_tmp += (internal_state_[2] < -kPi) * 2 * kPi;
-      state_(2) = theta_tmp;
-      state_(3) = internal_state_[3];
+    odeint::integrate(boost::ref(*this), internal_state_, 0.0, dt, dt);
 
-      state1 = state_;
-    } 
+    state_(0) = internal_state_[0];
+    state_(1) = internal_state_[1];
+    decimal_t theta_tmp = internal_state_[2];
+    //hzchzc
+    // theta_tmp -= (internal_state_[2]>= kPi) * 2 * kPi;
+    // theta_tmp += (internal_state_[2] < -kPi) * 2 * kPi;
+    state_(2) = theta_tmp;
+    state_(3) = internal_state_[3];
 
-    // For internal use, but needs to be public for odeint
-    typedef boost::array<double, 4> InternalState;
-    void operator()(const InternalState &x, InternalState &dxdt,
-                                  const double /* t */) {
+    state1 = state_;
+  }
 
-      dxdt[0] = cos(x[2]) * x[3];
-      dxdt[1] = sin(x[2]) * x[3];
-      dxdt[2] = tan(control_(0)) * x[3] / vp_.wheel_base();
-      dxdt[3] = control_(1);
-    }
+  // For internal use, but needs to be public for odeint
+  typedef boost::array<double, 4> InternalState;
+  void operator()(const InternalState &x, InternalState &dxdt,
+                  const double /* t */) {
 
+    dxdt[0] = cos(x[2]) * x[3];
+    dxdt[1] = sin(x[2]) * x[3];
+    dxdt[2] = tan(control_(0)) * x[3] / vp_.wheel_base();
+    dxdt[3] = control_(1);
+  }
 
-  private:
-  
-    void UpdateInternalState(void) {
-      internal_state_[0] = state_(0);
-      internal_state_[1] = state_(1);
-      internal_state_[2] = state_(2);
-      internal_state_[3] = state_(3);
-    }
+ private:
 
-    Eigen::Vector4d state_;
-    Eigen::Vector2d control_;
-    common::VehicleParam vp_;
-    InternalState internal_state_;
-  };
+  void UpdateInternalState(void) {
+    internal_state_[0] = state_(0);
+    internal_state_[1] = state_(1);
+    internal_state_[2] = state_(2);
+    internal_state_[3] = state_(3);
+  }
 
+  Eigen::Vector4d state_;
+  Eigen::Vector2d control_;
+  common::VehicleParam vp_;
+  InternalState internal_state_;
+};
 
 }
--- src/Plan/traj_planner/src/map_adapter.cpp
+++ src/Plan/traj_planner/src/map_adapter.cpp
@@ -12,19 +12,19 @@ bool TrajPlannerAdapter::IsValid() { return is_valid_; }
 
 decimal_t TrajPlannerAdapter::GetTimeStamp() { return map_->time_stamp(); }
 
-ErrorType TrajPlannerAdapter::GetEgoVehicle(Vehicle* vehicle) {
+ErrorType TrajPlannerAdapter::GetEgoVehicle(Vehicle *vehicle) {
   if (!is_valid_) return kWrongStatus;
   *vehicle = map_->ego_vehicle();
   return kSuccess;
 }
 
-ErrorType TrajPlannerAdapter::GetEgoState(State* state) {
+ErrorType TrajPlannerAdapter::GetEgoState(State *state) {
   if (!is_valid_) return kWrongStatus;
   *state = map_->ego_vehicle().state();
   return kSuccess;
 }
 
-ErrorType TrajPlannerAdapter::GetLocalReferenceLane(Lane* lane) {
+ErrorType TrajPlannerAdapter::GetLocalReferenceLane(Lane *lane) {
   if (!is_valid_) return kWrongStatus;
   auto ref_lane = map_->ego_behavior().ref_lane;
   if (!ref_lane.IsValid()) {
@@ -36,8 +36,8 @@ ErrorType TrajPlannerAdapter::GetLocalReferenceLane(Lane* lane) {
 }
 
 ErrorType TrajPlannerAdapter::GetForwardTrajectories(
-    std::vector<LateralBehavior>* behaviors,
-    vec_E<vec_E<common::Vehicle>>* trajs) {
+    std::vector<LateralBehavior> *behaviors,
+    vec_E<vec_E<common::Vehicle>> *trajs) {
   if (!is_valid_) return kWrongStatus;
   if (map_->ego_behavior().forward_behaviors.size() < 1) return kWrongStatus;
   *behaviors = map_->ego_behavior().forward_behaviors;
@@ -46,20 +46,20 @@ ErrorType TrajPlannerAdapter::GetForwardTrajectories(
 }
 
 ErrorType TrajPlannerAdapter::GetForwardTrajectories(
-    std::vector<LateralBehavior>* behaviors,
-    vec_E<vec_E<common::Vehicle>>* trajs,
-    vec_E<std::unordered_map<int, vec_E<common::Vehicle>>>* sur_trajs) {
+    std::vector<LateralBehavior> *behaviors,
+    vec_E<vec_E<common::Vehicle>> *trajs,
+    vec_E<std::unordered_map<int, vec_E<common::Vehicle>>> *sur_trajs) {
   if (!is_valid_) return kWrongStatus;
   if (map_->ego_behavior().forward_behaviors.size() < 1) return kWrongStatus;
   *behaviors = map_->ego_behavior().forward_behaviors;
   *trajs = map_->ego_behavior().forward_trajs;
   *sur_trajs = map_->ego_behavior().surround_trajs;
-  
+
   return kSuccess;
 }
 
 ErrorType TrajPlannerAdapter::GetEgoDiscretBehavior(
-    LateralBehavior* lat_behavior) {
+    LateralBehavior *lat_behavior) {
   if (!is_valid_) return kWrongStatus;
   if (map_->ego_behavior().lat_behavior == common::LateralBehavior::kUndefined)
     return kWrongStatus;
@@ -67,7 +67,7 @@ ErrorType TrajPlannerAdapter::GetEgoDiscretBehavior(
   return kSuccess;
 }
 
-ErrorType TrajPlannerAdapter::GetLaneByLaneId(const int lane_id, Lane* lane) {
+ErrorType TrajPlannerAdapter::GetLaneByLaneId(const int lane_id, Lane *lane) {
   if (!is_valid_) return kWrongStatus;
   auto semantic_lane_set = map_->semantic_lane_set();
   auto it = semantic_lane_set.semantic_lanes.find(lane_id);
@@ -79,7 +79,7 @@ ErrorType TrajPlannerAdapter::GetLaneByLaneId(const int lane_id, Lane* lane) {
   return kSuccess;
 }
 
-ErrorType TrajPlannerAdapter::GetEgoReferenceLane(Lane* lane) {
+ErrorType TrajPlannerAdapter::GetEgoReferenceLane(Lane *lane) {
   if (!is_valid_) return kWrongStatus;
   auto ref_lane = map_->ego_behavior().ref_lane;
   if (!ref_lane.IsValid()) {
@@ -90,51 +90,51 @@ ErrorType TrajPlannerAdapter::GetEgoReferenceLane(Lane* lane) {
   return kSuccess;
 }
 
-ErrorType TrajPlannerAdapter::GetObstacleMap(GridMap2D* grid_map) {
+ErrorType TrajPlannerAdapter::GetObstacleMap(GridMap2D *grid_map) {
   if (!is_valid_) return kWrongStatus;
   *grid_map = map_->obstacle_map();
   return kSuccess;
 }
-ErrorType TrajPlannerAdapter::CheckCollisionUsingGlobalPosition(Eigen::Vector2d& pos, bool* res){
+ErrorType TrajPlannerAdapter::CheckCollisionUsingGlobalPosition(Eigen::Vector2d &pos, bool *res) {
   if (!is_valid_) return kWrongStatus;
   map_->CheckCollisionUsingGlobalPosition(pos, res);
   return kSuccess;
 }
 ErrorType TrajPlannerAdapter::CheckIfCollision(
-    const common::VehicleParam& vehicle_param, const State& state, bool* res) {
+    const common::VehicleParam &vehicle_param, const State &state, bool *res) {
   if (!is_valid_) return kWrongStatus;
   map_->CheckCollisionUsingStateAndVehicleParam(vehicle_param, state, res);
   return kSuccess;
 }
 
 ErrorType TrajPlannerAdapter::CheckIfCollisionUsingPosAndYaw(
-    const common::VehicleParam& vehicle_param, const Eigen::Vector3d &state, bool* res) {
+    const common::VehicleParam &vehicle_param, const Eigen::Vector3d &state, bool *res) {
   if (!is_valid_) return kWrongStatus;
   map_->CheckCollisionUsingPosAndYaw(vehicle_param, state, res);
   return kSuccess;
 }
 
-ErrorType TrajPlannerAdapter::CheckIfCollisionUsingLine(const Eigen::Vector2d p1, 
-                                           const Eigen::Vector2d p2, bool* res, double checkl){
-  if (!is_valid_) return kWrongStatus;    
-  for(double dl = 0.0; dl < (p2-p1).norm(); dl+=checkl){
-    Eigen::Vector2d pos = (p2-p1)*dl/(p2-p1).norm()+p1;
-    map_->CheckCollisionUsingGlobalPosition(pos,res);  
-    if(*res){
+ErrorType TrajPlannerAdapter::CheckIfCollisionUsingLine(const Eigen::Vector2d p1,
+                                                        const Eigen::Vector2d p2, bool *res, double checkl) {
+  if (!is_valid_) return kWrongStatus;
+  for (double dl = 0.0; dl < (p2 - p1).norm(); dl += checkl) {
+    Eigen::Vector2d pos = (p2 - p1) * dl / (p2 - p1).norm() + p1;
+    map_->CheckCollisionUsingGlobalPosition(pos, res);
+    if (*res) {
       return kSuccess;
     }
   }
-  map_->CheckCollisionUsingGlobalPosition(p2,res);
+  map_->CheckCollisionUsingGlobalPosition(p2, res);
   return kSuccess;
 }
 ErrorType TrajPlannerAdapter::GetMovingObsTraj(
-      std::vector<std::vector<common::State>>* sur_trajs){
+    std::vector<std::vector<common::State>> *sur_trajs) {
   if (!is_valid_) return kWrongStatus;
   *sur_trajs = map_->movingObstraj();
   return kSuccess;
 }
 ErrorType TrajPlannerAdapter::GetObstacleGrids(
-    std::set<std::array<decimal_t, 2>>* obs_grids) {
+    std::set<std::array<decimal_t, 2>> *obs_grids) {
   if (!is_valid_) return kWrongStatus;
   *obs_grids = map_->obstacle_grids();
   return kSuccess;
